53571684e70d1b0741d3c376a97885c12ae17f77
Tafadzwa Muteke <muteket@gmail.com>
2019-04-22 15:22:29 +0200

    fixed: property POST authorization

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -15,6 +15,6 @@ export class AuthInterceptor implements HttpInterceptor {
 		const authRequest = req.clone({
 			headers: req.headers.set("Authorization", "Bearer " + authToken)
 		});
-		return next.handle(req);
+		return next.handle(authRequest);
 	}
 }
